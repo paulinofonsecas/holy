50cab4a941c913406bca73c55eef754ee6823b11
Paulino Fonseca <paulinofonsecass@gmail.com>
2026-05-07 15:20:32 +0100

    up

--- lib/shared/widgets/analysis_banner_overlay.dart
+++ lib/shared/widgets/analysis_banner_overlay.dart
@@ -1,4 +1,5 @@
 import 'package:eu_sou/features/deep_understanding/presentation/bloc/deep_understanding_bloc.dart';
+import 'package:eu_sou/shared/widgets/analysis_banner.dart';
 import 'package:eu_sou/shared/widgets/main_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
